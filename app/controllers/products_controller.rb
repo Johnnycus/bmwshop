c4bca76c971eec7b47c47215282112d782e6d713
Albert Itaev <ijohnnyzzz@gmail.com>
2015-08-21 18:58:38 +0000

    Create products controller and views, update model and add styles

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,47 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :update, :show]
+
   def index
+    @products = Product.all
   end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :overview, :description, :photo)
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   validates :name, presence: true
   validates :overview, presence: true, length: { minimum: 10, maximum: 250 }
   validates :description, presence: true
+
+  has_attached_file :photo, styles: { preview: "280x171>" },
+                    default_url:  "/assets/products/:id/:style/:basename.:extension",
+                    path: ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+
+  validates_attachment_presence :photo
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
