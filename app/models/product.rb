d6747013be8a2f00d777898227446ff44c3dbd71
Albert Itaev <ijohnnyzzz@gmail.com>
2015-08-22 12:58:46 +0000

    Add show car page, some navbar fixes. Features for index products page

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,7 @@ class Product < ActiveRecord::Base
   validates :overview, presence: true, length: { minimum: 10, maximum: 250 }
   validates :description, presence: true
 
-  has_attached_file :photo#,
-                    #default_url:  "/assets/products/:id/:style/:basename.:extension",
-                    #path: ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+  has_attached_file :photo
 
   validates_attachment_presence :photo
   validates_attachment_content_type :photo, content_type: ['image/jpeg', 'image/png']
